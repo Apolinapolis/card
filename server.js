645b3a80ce4b9b59a5b2efa73805ccafd82876e0
Apolinapolis <larra12@ya.ru>
2025-04-23 21:33:03 +0300

    soft non hover effect for lang-toogle

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
-const themeToggleText = document.getElementById("theme-toggle");
-const themeToggleIcon = document.getElementById("theme-toggle-icon");
+// Локализация
+const translations = {
+    en: {
+        nav: {
+            bookshelf: "Bookshelf",
+            works: "Works",
+            mode: "Mode"
+        },
+        h2: "Efficiency-improving software",
+        langToggle: "ru/EN"
+    },
+    ru: {
+        nav: {
+            bookshelf: "Книги",
+            works: "Работы",
+            mode: "Режим"
+        },
+        h2: "Нечто не имеющее ничего неинтересного",
+        langToggle: "RU/en"
+    }
+};
 
-
+//функция установки цветовой темы
 function toggleTheme() {
     document.body.classList.toggle("dark-theme");
 
@@ -9,17 +28,35 @@ function toggleTheme() {
     localStorage.setItem("theme", isDarkTheme ? "dark" : "light");
 }
 
-if (themeToggleText) {
-    themeToggleText.addEventListener("click", toggleTheme);
+// Функция подмены текста
+function updateText(language) {
+    document.querySelectorAll('[data-key]').forEach(item => {
+        const key = item.getAttribute('data-key');
+        if (translations[language].nav[key]) {
+            item.textContent = translations[language].nav[key];
+        }
+    });
+
+    document.querySelector('h2').textContent = translations[language].h2;
+    document.querySelector('.lang-toggle').textContent = translations[language].langToggle;
 }
 
-if (themeToggleIcon) {
-    themeToggleIcon.addEventListener("click", toggleTheme);
+//Функция переключения локализации
+function toggleLanguage() {
+    let currentLanguage = localStorage.getItem('language') || 'en';
+    const newLanguage = currentLanguage === 'en' ? 'ru' : 'en';
+    localStorage.setItem('language', newLanguage);
+    updateText(newLanguage);
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+// Инициализация при загрузке страницы
+document.addEventListener('DOMContentLoaded', () => {
+    const savedLanguage = localStorage.getItem('language') || 'en';
     const savedTheme = localStorage.getItem("theme");
+
     if (savedTheme === "dark") {
         document.body.classList.add("dark-theme");
     }
+
+    updateText(savedLanguage);
 });
